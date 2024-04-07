34634b435e3d264e9713db89635d419c1b54ba08
Zaraksh Rahman <zarakshR@pm.me>
2024-04-07 06:13:41 +0100

    refactor: CycleEntry: enums for terrain and tempo
    also reformatted

--- src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
+++ src/main/java/com/stir/cscu9t4practical1/CycleEntry.java
@@ -4,28 +4,51 @@ import java.util.TimeZone;
 
 public class CycleEntry extends Entry {
 
-    private final String terrain;
+    private final Terrain terrain;
+    private final Tempo tempo;
 
-    public String getTerrain() {
+    public CycleEntry(String n, int d, int m, int y, int h, int min, int s, TimeZone tz, float dist, Terrain terrain,
+                      Tempo tempo) {
+        super(n, d, m, y, h, min, s, tz, dist);
+        this.terrain = terrain;
+        this.tempo = tempo;
+    }
+
+    public Terrain getTerrain() {
         return terrain;
     }
 
-    public String getTempo() {
+    public Tempo getTempo() {
         return tempo;
-    }
-
-    private final String tempo;
-
-    public CycleEntry(String n, int d, int m, int y, int h, int min, int s, TimeZone tz, float dist, String terraim,
-                      String tempo) {
-        super(n, d, m, y, h, min, s, tz, dist);
-        this.terrain = terraim;
-        this.tempo = tempo;
     }
 
     @Override
     public String formattedEntry() {
         return getName() + " cycled " + formattedDistance() + " in " + formattedTime() + " " +
                 "on " + formattedDate() + " on " + getTerrain() + " at " + getTempo() + " tempo\n";
+    }
+
+    public enum Terrain {
+        ASPHALT,
+        GRAVEL,
+        MOUNTAIN;
+
+        // the tests expect lowercase terrain names
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase();
+        }
+    }
+
+    public enum Tempo {
+        FAST,
+        MODERATE,
+        SLOW;
+
+        // the tests expect lowercase tempo names
+        @Override
+        public String toString() {
+            return super.toString().toLowerCase();
+        }
     }
 }
