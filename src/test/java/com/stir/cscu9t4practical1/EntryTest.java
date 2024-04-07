a3d6b4d1dfeb040d5200196d97de2588f88a0977
Zaraksh Rahman <zarakshR@pm.me>
2024-04-07 06:31:55 +0100

    refactor: EntryTest
    use a single static instance of the class to test
    remove lots of superflous code

--- src/test/java/com/stir/cscu9t4practical1/EntryTest.java
+++ src/test/java/com/stir/cscu9t4practical1/EntryTest.java
@@ -17,6 +17,7 @@ public class EntryTest {
 
     // use a fixed time zone for tests
     final static TimeZone tz = TimeZone.getTimeZone("GMT");
+    final static Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
 
     public EntryTest() {
     }
@@ -42,11 +43,7 @@ public class EntryTest {
      */
     @Test
     public void testGetName() {
-        System.out.println("getName");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        String expResult = "Alice";
-        String result = instance.getName();
-        assertEquals(expResult, result);
+        assertEquals("Alice", instance.getName());
     }
 
     /**
@@ -54,11 +51,7 @@ public class EntryTest {
      */
     @Test
     public void testGetDay() {
-        System.out.println("getDay");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 1;
-        int result = instance.getDay();
-        assertEquals(expResult, result);
+        assertEquals(1, instance.getDay());
     }
 
     /**
@@ -66,11 +59,7 @@ public class EntryTest {
      */
     @Test
     public void testGetMonth() {
-        System.out.println("getMonth");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 2;
-        int result = instance.getMonth();
-        assertEquals(expResult, result);
+        assertEquals(2, instance.getMonth());
     }
 
     /**
@@ -78,11 +67,7 @@ public class EntryTest {
      */
     @Test
     public void testGetYear() {
-        System.out.println("getYear");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 2003;
-        int result = instance.getYear();
-        assertEquals(expResult, result);
+        assertEquals(2003, instance.getYear());
     }
 
     /**
@@ -90,11 +75,7 @@ public class EntryTest {
      */
     @Test
     public void testGetHour() {
-        System.out.println("getHour");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 0;
-        int result = instance.getHour();
-        assertEquals(expResult, result);
+        assertEquals(0, instance.getHour());
     }
 
     /**
@@ -102,11 +83,7 @@ public class EntryTest {
      */
     @Test
     public void testGetMin() {
-        System.out.println("getMin");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 16;
-        int result = instance.getMin();
-        assertEquals(expResult, result);
+        assertEquals(16, instance.getMin());
     }
 
     /**
@@ -114,11 +91,7 @@ public class EntryTest {
      */
     @Test
     public void testGetSec() {
-        System.out.println("getSec");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        int expResult = 7;
-        int result = instance.getSec();
-        assertEquals(expResult, result);
+        assertEquals(7, instance.getSec());
     }
 
     /**
@@ -126,11 +99,7 @@ public class EntryTest {
      */
     @Test
     public void testGetDistance() {
-        System.out.println("getDistance");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
-        double expResult = 3.0;
-        double result = instance.getDistance();
-        assertEquals(result, expResult, 0.0);
+        assertEquals(3.0, instance.getDistance(), 0.0);
     }
 
     /**
@@ -138,11 +107,8 @@ public class EntryTest {
      */
     @Test
     public void testGetEntry() {
-        System.out.println("getEntry");
-        Entry instance = new Entry("Alice", 1, 2, 2003, 0, 16, 7, tz, 3);
         String expResult = "Alice had a training session, achieving distance 3.0 km in 0:16:7 on 1/2/2003\n";
-        String result = instance.formattedEntry();
-        assertEquals(expResult, result);
+        assertEquals(expResult, instance.formattedEntry());
     }
 
 }
