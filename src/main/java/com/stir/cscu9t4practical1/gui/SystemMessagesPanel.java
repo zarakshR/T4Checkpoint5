2ef10d8f4d08b8afe500e27e95ec8a2462b7dbed
Zaraksh Rahman <zarakshR@pm.me>
2024-04-08 19:29:56 +0100

    SystemMessagesPanel: autoscroll + limit to 3 rows
    SystemMessagesPanel now limits itself to 3 rows, this prevents the text
    area from growing and taking up half the screen
    
    it also auto scrolls to the bottom when printed to

--- src/main/java/com/stir/cscu9t4practical1/gui/SystemMessagesPanel.java
+++ src/main/java/com/stir/cscu9t4practical1/gui/SystemMessagesPanel.java
@@ -9,12 +9,16 @@ final class SystemMessagesPanel extends JScrollPane {
 
     public SystemMessagesPanel() {
         super(messageConsole);
+        messageConsole.setRows(3); // we don't want more than 3 lines in the system console at a time
         messageConsole.setEditable(false);
         messageConsole.setFocusable(false);
-        messageConsole.setText("System Messages");
+        println("System Messages");
     }
 
     public void println(String s) {
         messageConsole.append("\n" + s);
+        // scroll to the bottom everytime we append a message
+        JScrollBar scrollBar = getVerticalScrollBar();
+        scrollBar.setValue(scrollBar.getMaximum());
     }
 }
