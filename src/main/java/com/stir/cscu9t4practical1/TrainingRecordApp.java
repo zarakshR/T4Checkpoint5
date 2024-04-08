833bba0f38517425895ab5921d1d6a06d9bc9c4c
Zaraksh Rahman <zarakshR@pm.me>
2024-04-08 11:22:45 +0100

    inject dummy data in TrainingRecordApp for testing purposes

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
@@ -1,13 +1,24 @@
 package com.stir.cscu9t4practical1;
 
+import com.stir.cscu9t4practical1.entries.RunEntry;
+import com.stir.cscu9t4practical1.entries.TrainingRecord;
 import com.stir.cscu9t4practical1.gui.MainFrame;
+
+import java.time.ZonedDateTime;
 
 public class TrainingRecordApp {
 
     private static MainFrame mainFrame;
+    private static final TrainingRecord trainingRecord = new TrainingRecord();
 
     public static void main(String[] args) {
-        mainFrame = new MainFrame();
+
+        trainingRecord.addEntry(new RunEntry("Alice", ZonedDateTime.now(), 10.0));
+        trainingRecord.addEntry(new RunEntry("Bob", ZonedDateTime.now(), 10.0));
+        trainingRecord.addEntry(new RunEntry("Charlie", ZonedDateTime.now(), 10.0));
+        trainingRecord.addEntry(new RunEntry("David", ZonedDateTime.now(), 10.0));
+
+        mainFrame = new MainFrame(trainingRecord);
         mainFrame.setVisible(true);
     }
 }
