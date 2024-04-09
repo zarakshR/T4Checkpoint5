9cad6e822f7060b450bc2c51fc9587592b066020
Zaraksh Rahman <zarakshR@pm.me>
2024-04-09 10:09:41 +0100

    add action listening for search requests
    currenlt jsut prints to stdout

--- src/main/java/com/stir/cscu9t4practical1/gui/SearchPanel.java
+++ src/main/java/com/stir/cscu9t4practical1/gui/SearchPanel.java
@@ -2,62 +2,81 @@ package com.stir.cscu9t4practical1.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // TODO: Write tests for this
-final class SearchPanel extends JPanel {
+final class SearchPanel extends JPanel implements ActionListener {
+
+    private final NameSearchPanel nameSearchPanel = new NameSearchPanel(this);
+    private final DateSearchPanel dateSearchPanel = new DateSearchPanel(this);
 
     SearchPanel() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-
-        NameSearchPanel nameSearchPanel = new NameSearchPanel();
-        DateSearchPanel dateSearchPanel = new DateSearchPanel();
-
         add(nameSearchPanel);
         add(dateSearchPanel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == nameSearchPanel.searchByNameButton) {
+            System.out.println("name search = " + nameSearchPanel.nameField.getTextField().getText());
+        }
+        if (e.getSource() == nameSearchPanel.weeklyDistanceButton) {
+            System.out.println("weekly distance = " + nameSearchPanel.nameField.getTextField().getText());
+        }
+        if (e.getSource() == dateSearchPanel.searchByDateButton) {
+            System.out.println("date search = " + dateSearchPanel.dateField.getTextField().getText());
+        }
     }
 
     // We need these two to be separate panels, otherwise the layout looks horrible
     private static final class NameSearchPanel extends JPanel {
 
-        NameSearchPanel() {
+        private final LabelledTextPanel nameField = new LabelledTextPanel("Name");
+        private final JButton searchByNameButton = new JButton("Search By Name");
+        private final JButton weeklyDistanceButton = new JButton("Calculate Weekly Distance");
+
+        NameSearchPanel(ActionListener controller) {
             setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
 
-            LabelledTextPanel nameField = new LabelledTextPanel("Name");
             c.gridx = 0;
             c.gridy = 0;
             c.gridwidth = 2;
             add(nameField, c);
 
-            JButton searchByNameButton = new JButton("Search By Name");
             c.gridx = 0;
             c.gridy = 1;
             c.gridwidth = 1;
             add(searchByNameButton, c);
+            searchByNameButton.addActionListener(controller);
 
-            JButton weeklyDistanceButton = new JButton("Calculate Weekly Distance");
             c.gridx = 1;
             c.gridy = 1;
             c.gridwidth = 1;
             add(weeklyDistanceButton, c);
+            weeklyDistanceButton.addActionListener(controller);
         }
     }
 
     private static final class DateSearchPanel extends JPanel {
 
-        DateSearchPanel() {
+        private final LabelledTextPanel dateField = new LabelledTextPanel("Date");
+        private final JButton searchByDateButton = new JButton("Search By Date");
+
+        DateSearchPanel(ActionListener controller) {
             setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
 
-            LabelledTextPanel dateField = new LabelledTextPanel("Date");
             c.gridx = 0;
             c.gridy = 0;
             add(dateField, c);
 
-            JButton searchByDateButton = new JButton("Search By Date");
             c.gridx = 0;
             c.gridy = 1;
             add(searchByDateButton, c);
+            searchByDateButton.addActionListener(controller);
         }
     }
 }
