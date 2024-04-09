9891846698ec38ab4fdce71100d282341adbac30
Zaraksh Rahman <zarakshR@pm.me>
2024-04-09 20:35:16 +0100

    EXIT_ON_CLOSE

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
@@ -4,6 +4,7 @@ import com.stir.cscu9t4practical1.entries.RunEntry;
 import com.stir.cscu9t4practical1.entries.TrainingRecord;
 import com.stir.cscu9t4practical1.gui.MainFrame;
 
+import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDateTime;
 
@@ -24,6 +25,7 @@ public final class TrainingRecordApp {
         trainingRecord.addEntry(new RunEntry("David", LocalDateTime.now(), 10.0));
 
         MainFrame mainFrame = new MainFrame(trainingRecord);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setPreferredSize(new Dimension(800, 400));
         mainFrame.setTitle("Training Record Manager Ultimate Pro Edition");
         mainFrame.setVisible(true);
