a3021a5fd1a8af7efaf52142dfcab57570ede518
Zaraksh Rahman <zarakshR@pm.me>
2024-04-09 17:03:50 +0100

    MainFrame calls setPreferredSize

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecordApp.java
@@ -4,6 +4,7 @@ import com.stir.cscu9t4practical1.entries.RunEntry;
 import com.stir.cscu9t4practical1.entries.TrainingRecord;
 import com.stir.cscu9t4practical1.gui.MainFrame;
 
+import java.awt.*;
 import java.time.LocalDateTime;
 
 public class TrainingRecordApp {
@@ -19,6 +20,7 @@ public class TrainingRecordApp {
         trainingRecord.addEntry(new RunEntry("David", LocalDateTime.now(), 10.0));
 
         MainFrame mainFrame = new MainFrame(trainingRecord);
+        mainFrame.setPreferredSize(new Dimension(800, 400));
         mainFrame.setVisible(true);
     }
 }
